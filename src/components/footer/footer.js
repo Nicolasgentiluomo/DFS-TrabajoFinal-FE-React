f2452787ff51a49a6d6c636f16e55fc28705115d
Nicolas <ngentiluomo@hotmail.com>
2024-09-06 23:28:52 -0300

    Home Completed

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,6 +1,28 @@
+import './footer.css';
+
 function Footer(){
     return (
-        <div></div>
+        <footer className="footer">
+        <div className="footer-content">
+            <h3>Contacto</h3>
+            <p><i className="fas fa-envelope"></i> contacto@gmail.com</p>
+            <p><i className="fas fa-phone"></i> +54 123 456 789</p>
+            <a href="https://wa.me/541131503073?text=Hola!%20Quiero%20hacerte%20una%20consulta...">
+                <i className="fa-brands fa-whatsapp"></i> Haceme tu pregunta!</a>
+        </div>
+
+        <div className="footer-content">
+            <h3>Síguenos</h3>
+            <div className="social-icons">
+                <a href="#" className="icon-link"><i className="fab fa-facebook-f"></i></a>
+                <a href="#" className="icon-link"><i className="fab fa-instagram"></i></a>
+            </div>
+        </div>
+            
+        <div className="footer-content">
+            <p>Copyright &copy; 2024, Todos los derechos reservados</p>
+        </div>
+    </footer>
     )
 }
 
--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,6 +1,46 @@
+import Producto from '../../assets/Producto2.png';
+import Textura from '../../assets/Textura1.png';
+import Trabajo from '../../assets/Trabajando.png';
+import './home.css'
+
+
 function Home(){
     return (
-        <div className="home"></div>
+        <section className="main-section">
+
+            <a href="hola" className="boxContainer1">
+                <div className="text1">
+                    <h1>Nosotros</h1>
+                    <p>
+                        Somos un emprendimiento que basa sus servicios y productos
+                        en la elaboracion de muebles, herrajes, espejos e iluminacion.
+                    </p>
+                    <p>Hace click en la imagen para saber mas de nosotros</p>
+                </div>
+                <img href="hola" src={Trabajo} alt=""/>
+            </a>
+
+            <a href="hola2" className="boxContainer2"> 
+                <div className="text2">
+                    <h1>Texturas</h1>
+                    <p>Realizamos diferentes tipos de texturas tanto al hierro
+                        como a la madera. Podes elegir la textura que mas te guste!
+                    </p>
+                    <p>Hace click en la imagen para ver algunas de las texturas que ofrecemos</p>
+                </div>
+                <img src={Textura} alt=""/>
+            </a>   
+
+            <a href="./Products.html" className="boxContainer1">
+                <div className="text1">
+                    <h1>Productos</h1>
+                    <p>Queres ver algunos de los productos que tenemos a la venta?</p>
+                    <p>Hace click para poder verlos, y si te gustan, comprarlos!</p>
+                </div>
+                <img src={Producto} alt=""/>
+            </a>
+            
+        </section> 
         );
 };
 
